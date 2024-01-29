974ce1e0afdef068d5ce6ded1b5a91cca2c19a65
MaximCosta <mxmcosta@gmail.com>
2024-01-29 11:41:09 +0100

    User

---
+++ tests/Unit/UserCreateTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserCreateTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testCreateUserWithMissingUsername()
+    {
+        $response = $this->post('/api/register', [
+            'email' => 'test@test.com',
+            'password' => 'test1',
+        ]);
+        $response->assertStatus(400);
+    }
+
+    public function testCreateUserWithMissingEmail()
+    {
+        $response = $this->post('/api/register', [
+            'username' => 'test',
+            'password' => 'test1',
+        ]);
+        $response->assertStatus(400);
+    }
+
+    public function testCreateUserWithMissingPassword()
+    {
+        $response = $this->post('/api/register', [
+            'username' => 'test',
+            'email' => 'test1@test.com',
+        ]);
+        $response->assertStatus(400);
+    }
+
+    public function testCreateUserWithExistingEmail()
+    {
+        User::factory()->create([
+            'email' => 'test2@test.com'
+        ]);
+        $response = $this->post('/api/register', [
+            'username' => 'test',
+            'email' => 'test2@test.com',
+            'password' => 'test1',
+        ]);
+        $response->assertStatus(400);
+    }
+
+    public function testCreateUserWithValidData()
+    {
+        $response = $this->post('/api/register', [
+            'username' => 'test',
+            'email' => 'test1@test.com',
+            'password' => 'test1',
+        ]);
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('users', [
+            'email' => 'test1@test.com'
+        ]);
+    }
+
+}
